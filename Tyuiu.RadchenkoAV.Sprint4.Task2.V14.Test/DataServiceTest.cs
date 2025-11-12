d1081d5e68123c3ae80a1a4138a7732ca533de3a
alisa69 <aliska132007@gmail.ru>
2025-11-13 00:40:21 +0500

    Task2

---
+++ Tyuiu.RadchenkoAV.Sprint4.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadchenkoAV.Sprint4.Task2.V14.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidMulti()
+        {
+            DataService ds = new DataService();
+
+            int[] numbers = { 4, 8, 1, 7, 7, 3, 1, 5, 8, 2, 2, 1 };
+
+            int res = ds.Calculate(numbers);
+            int wait = 735;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
