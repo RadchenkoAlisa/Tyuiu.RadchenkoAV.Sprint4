1ea9eefcbe41a463f2b2dd990a7dfffbb668e8d4
alisa69 <aliska132007@gmail.ru>
2025-11-13 21:50:05 +0500

    Task4

---
+++ Tyuiu.RadchenkoAV.Sprint4.Task4.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task4.V5.Lib
+{
+    public class DataService : ISprint4Task4V5
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+    }
+}
