9903e240aeb1f9885a4d87d25fa0af85e1b9df92
alisa69 <aliska132007@gmail.ru>
2025-11-13 19:42:02 +0500

    Task3

---
+++ Tyuiu.RadchenkoAV.Sprint4.Task3.V17.Lib/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task3.V17.Lib
+{
+    public class DataService : ISprint4Task3V17
+    {
+        public int Calculate(int[,] array)
+        {
+            // Сумма элементов в четвертом столбце
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, 3];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint4.Task3.V17.Test/DataService.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.RadchenkoAV.Sprint4.Task3.V17.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task3.V17.Test
+{
+    [TestClass]
+    public sealed class DataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
