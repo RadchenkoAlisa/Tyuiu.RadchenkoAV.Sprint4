89d3658c0d41f0432baeae46bb115348ec902797
alisa69 <aliska132007@gmail.ru>
2025-11-13 23:42:37 +0500

    Task5

---
+++ Tyuiu.RadchenkoAV.Sprint4.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.RadchenkoAV.Sprint4.Task5.V5.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[3, 3] { { 1, -5, 3 },
+                                           { 2, -5, 4 },
+                                           { 3, -5, 5 } };
+            int res = ds.Calculate(matrix);
+            int wait = 18;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint4.Task5.V5/Program.cs
@@ -0,0 +1,58 @@
+﻿using Tyuiu.RadchenkoAV.Sprint4.Task5.V5.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнил: РадченкоАВ | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: РадченкоАВ | РППБ-25-1                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от 5 до -8. Найдите сумму             *");
+            Console.WriteLine("* положительных элементов.                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int RowsOfArray = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int ColumnsOfArray = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            int[,] matrix = new int[RowsOfArray, ColumnsOfArray];
+            for (int i = 0; i < RowsOfArray; i++)
+            {
+                for (int j = 0; j < ColumnsOfArray; j++)
+                {
+                    matrix[i, j] = rnd.Next(-5, 9);
+                }
+            }
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < RowsOfArray; i++)
+            {
+                for (int j = 0; j < ColumnsOfArray; j++)
+                {
+                    Console.Write($"{matrix[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+            int Result = ds.Calculate(matrix);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($" Сумма положительных элементов массива = {Result}");
+        }
+    }
+}
