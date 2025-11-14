532b3d5a93cacbbf9b897a2a7ca76740baf76dd2
alisa69 <aliska132007@gmail.ru>
2025-11-14 21:12:25 +0500

    Task7

---
+++ Tyuiu.RadchenkoAV.Sprint4.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.RadchenkoAV.Sprint4.Task7.V26.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint4.Task7.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "351268459614723";
+            int r = 3;
+            int c = 5;
+            int wait = 18432;
+            int res = ds.Calculate(r, c, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint4.Task7.V26.Test/ISprint4Task7V26.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.RadchenkoAV.Sprint4.Task7.V26.Lib
+{
+    public interface ISprint4Task7V26
+    {
+    }
+}
